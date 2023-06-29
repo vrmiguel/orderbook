f6f30201210fcbb8358187ee8c3aa3ce01d4e2c3
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2023-06-29 02:53:00 -0300

    feat(front): interact with the backend through Axios

---
+++ frontend/src/api.ts
@@ -0,0 +1,42 @@
+import axios, { AxiosResponse } from "axios";
+
+export type OrderCreationResponse = {
+    uuid: string;
+}
+
+export type Order = {
+    id: string;
+    quantity: number;
+    price: number;
+    side: OrderSide;
+}
+
+// The base address of our backend service
+const baseUrl = process.env.BASE_URL || 'http://127.0.0.1:8080';
+
+export type OrderSide = 'bid' | 'ask';
+
+const api = axios.create({
+    baseURL: baseUrl,
+});
+
+async function createOrder(quantity: number, price: number, orderSide: OrderSide): Promise<string> {
+    const endpoint = orderSide === 'bid' ? '/orders/bids' : '/orders/asks';
+    const requestBody = { quantity, price };
+
+    try {
+        const response = await api.post<OrderCreationResponse>(endpoint, requestBody);
+        return response.data.uuid;
+    } catch (error) {
+        throw new Error(`Failed to create ${orderSide.toLowerCase()} order: ${error}.`);
+    }
+}
+
+async function getOrders(): Promise<Order[]> {
+    try {
+        const response: AxiosResponse<{ orders: Order[] }> = await api.get('/orders');
+        return response.data.orders;
+    } catch (error) {
+        throw new Error(`Failed to fetch orders: ${error}.`);
+    }
+}
